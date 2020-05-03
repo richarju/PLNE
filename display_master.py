f67b7d58ec0add628d8e066ac4acfc98601cdfe3
richarju <48989838+richarju@users.noreply.github.com>
2020-05-03 18:00:51 +0200

    ca marche avec nouvel afficheur

--- display_master.py
+++ display_master.py
@@ -3,40 +3,50 @@ import matplotlib.lines as line
 import objects
 
 
-def display_planning_per_vehicle(list_of_v, pb):
+def display_planning_per_vehicle(pb):
     """
-    :param list_of_v: truc
     :param pb: problem solved
     :return: a matplotlib display that shows the planning for each vehicle
     """
-    scale_n = len(list_of_v)
+    scale_n = len(pb.vehicles)
     color_list = ['red', 'blue', 'green', 'black', 'magenta', 'darkred', 'darkblue', 'orange', 'yellow', 'cyan']
     fig, ax = plt.subplots()
     positions = list()
     label = list()
     parkings_used = list()
-    solution_wo_nvr = [vehicle for vehicle in list_of_v if vehicle.type.name != 'NVR']
-    max_y = len(solution_wo_nvr) + 0.01 * scale_n
-    for i, vehicle in enumerate(solution_wo_nvr):
+    solution_wo_nvr = [vehicle for vehicle in pb.vehicles if vehicle.type.name != 'NVR']
+    nvr_vehicles = [vehicle for vehicle in pb.vehicles if vehicle.type.name == 'NVR']
+    n_n_nvr = len(solution_wo_nvr)
+    solution_final = solution_wo_nvr + nvr_vehicles
+    max_y = len(solution_final) + 0.01 * scale_n
+    for i, vehicle in enumerate(solution_final):
         positions.append(i)
-        label.append('APV' + str(i+1) + ' ({})'.format(vehicle.type.name))
+        if i < n_n_nvr:
+            label.append('APV' + str(i+1) + ' ({})'.format(vehicle.type.name))
+        else:
+            label.append('NO VEHICLE')
         for task in vehicle.tasks:
-            beg = task.t_i
-            end = task.t_i + task.d_i
-            parkings_used.append(task.airplane.parking)
-            ax.plot([beg, end], [i, i], linewidth=2, color=color_list[task.airplane.parking-1],
-                    label='parking '+str(task.airplane.parking))
-            ax.text(beg + 2.5, i + 0.01 * scale_n, '{} - {}'.format(task.type.name, task.airplane.fl_nbr),
-                    ha="center", va="center", size=7, weight="bold")
-            ax.text(beg, i - 0.012 * scale_n, '{}'.format(beg), ha="center", va="center", size=6)
-            ax.text(end, i - 0.012 * scale_n, '{}'.format(end), ha="center", va="center", size=6)
+            if task.type.name not in ['In', 'Ob']:
+                beg = task.t_i
+                end = task.t_i + task.d_i
+                parkings_used.append(task.airplane.parking)
+                if task.type.name not in ['Db', 'Bd']:
+                    ax.plot([beg, end], [i, i], linewidth=2, color=color_list[task.airplane.parking-1],
+                            label='parking '+str(task.airplane.parking))
+                else:
+                    ax.plot([beg, end], [i, i], linewidth=2, color='grey')
+                ax.text(beg + 2.5, i + 0.013 * scale_n, '{} - {}'.format(task.type.name, task.airplane.fl_nbr),
+                            ha="center", va="center", size=7, weight="bold")
+                ax.text(beg, i - 0.013 * scale_n, '{}'.format(beg), ha="center", va="center", size=6)
+                ax.text(end, i - 0.013 * scale_n, '{}'.format(end), ha="center", va="center", size=6)
+
     for flight in pb.flights:
         ax.plot([flight.m_a-0.5, flight.m_a-0.5], [-0.5, max_y + pb.flights.index(flight) % 2],
                 linewidth=0.8, color=color_list[flight.parking-1])
-        ax.text(flight.m_a, max_y + pb.flights.index(flight) % 2, 'EIBT {}'.format(flight.fl_nbr), size=7, ha="center")
+        ax.text(flight.m_a, max_y + pb.flights.index(flight) % 2, 'In Block {}'.format(flight.fl_nbr), size=7, ha="center")
         ax.plot([flight.m_d, flight.m_d], [-0.5, max_y + pb.flights.index(flight) % 2],
                 linewidth=0.8, color=color_list[flight.parking-1])
-        ax.text(flight.m_d, max_y + pb.flights.index(flight) % 2, 'EOBT {}'.format(flight.fl_nbr), size=7, ha="center")
+        ax.text(flight.m_d, max_y + pb.flights.index(flight) % 2, 'Out Block {}'.format(flight.fl_nbr), size=7, ha="center")
     ax.grid(axis='y', color='grey')
     plt.yticks(positions, label, size=7)
     labels = ['parking '+str(p) for p in list(set(parkings_used))]
@@ -44,5 +54,5 @@ def display_planning_per_vehicle(list_of_v, pb):
     ax.legend(handles, labels, prop={'size': 7}, loc=4)
     plt.xlabel('Time (min)')
     plt.ylabel('Airport Vehicles (APV)')
-    plt.title("Airport Vehicles Fleet Planning")
+    plt.title("Airport Vehicles Fleet Planning - PLNE Solved")
     plt.show()
--- main.py
+++ main.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         x, t = solver.solve_model_pulp(pb)
         pb.make_array_after_plne(x, t)
         pb.fleet_from_plne(x, t)
-        disp.display_planning_per_vehicle(pb.vehicles, pb)
+        disp.display_planning_per_vehicle(pb)
